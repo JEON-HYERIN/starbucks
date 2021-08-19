3e0be6b84dbdf34ab267940091fc6de6e3ca7c13
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-19 21:18:22 +0900

    24 - Youtube iframe API (2)

--- 0810/js/main.js
+++ 0810/js/main.js
@@ -61,9 +61,9 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, //슬라이드 사이 여백
   centeredSlides: true, //1번 슬라이드가 가운데 보이기
   loop: true,
-  // autoplay: {
-  //   delay: 5000 //기본값:3000(3초)
-  // },
+  autoplay: {
+    delay: 5000 //기본값:3000(3초)
+  },
   pagination: {
     el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
     clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
