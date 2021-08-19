28057a90dfe641be7e9074067064cd2e84e8f1ce
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-19 10:41:41 +0900

    16 - 공지사항

--- 0810/js/main.js
+++ 0810/js/main.js
@@ -20,7 +20,7 @@ searchInputEl.addEventListener('blur', function () {
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if (window.scrollY > 500) {
     //배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
